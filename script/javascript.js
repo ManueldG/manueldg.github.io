7516a969f6497677763cfc76b0f7351a8772a7cc
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2024-11-26 23:14:54 +0100

    update

---
+++ script/javascript.js
@@ -0,0 +1,16 @@
+
+// Custom effects with .animate()
+$( "#testo" ).animate(
+    {
+        left: "+=50",
+        opacity: 0.25
+    },
+ 
+    // Duration
+    300,
+ 
+    // Callback to invoke when the animation is finished
+    function() {
+        console.log( "done!" );
+    }
+);
