9916df2cb6a0a3dcc9867c039f42940d7a6e7af0
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2024-11-27 20:27:35 +0100

    update

--- script/javascript.js
+++ script/javascript.js
@@ -13,5 +13,17 @@ $( "#testo" ).animate(
     function() {
         console.log( "done!" );
     }
+).animate({
+    right: "+=50",
+    opacity: 1
+},
+
+// Duration
+300,
+
+// Callback to invoke when the animation is finished
+function() {
+    console.log( "done!" );
+}
 );
 
